fd5eec3f7d8aee7fceb904d5217302c5001d425d
Tristan Jogminas <tristan.jogminas@gmail.com>
2020-01-20 20:13:02 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Tristan Jogminas on 1/20/20.
+//  Copyright © 2020 tristanj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
