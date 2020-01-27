60fe98dd4cfe2ae000016c69a850c82842f265ff
Tristan Jogminas <tristan.jogminas@gmail.com>
2020-01-26 18:03:27 -0800

    problem with ImageViewUI

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Tristan Jogminas on 1/26/20.
+//  Copyright © 2020 tristanj. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
